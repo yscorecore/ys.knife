cb0f31e4ad6b91a63a9447dda3c14e35afd1e36a
ypb <obpt@sina.com>
2019-12-26 23:21:12 +0800

    support symbol package

--- .github/scripts/pushpack.sh
+++ .github/scripts/pushpack.sh
@@ -4,4 +4,8 @@ NUGET_APIKEY=$2
 for file in `ls $PUBLISH_FOLDER/*.nupkg`
 do
     dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate
+done
+for file in `ls $PUBLISH_FOLDER/*.snupkg`
+do
+    dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate
 done        
