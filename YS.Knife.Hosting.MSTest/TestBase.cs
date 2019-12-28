799c2102de6205adf56a724e394e7d744c7f6231
ypb <obpt@sina.com>
2019-12-28 20:21:39 +0800

    add hosting mstest TestBase

---
+++ YS.Knife.Hosting.MSTest/TestBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Knife.Hosting.MSTest
+{
+    public abstract class TestBase
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            this.OnSetup();
+        }
+        protected IHost host;
+        protected virtual void OnSetup()
+        {
+            this.host = Host.CreateHost();
+        }
+
+        protected virtual void OnTearDown()
+        {
+            if (host != null)
+            {
+                this.host.Dispose();
+            }
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            this.OnTearDown();
+        }
+        public T Get<T>()
+        {
+            return this.host.Services.GetService<T>();
+        }
+    }
+
+    public class TestBase<T> : TestBase
+    {
+        public TestBase()
+        {
+            this.instanceFactory = new Lazy<T>(this.Get<T>, true);
+        }
+        private Lazy<T> instanceFactory;
+        protected T Instance 
+        {
+            get
+            {
+                return instanceFactory.Value;
+            }
+        }
+
+    }
+
+}
