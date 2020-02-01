e7963934adeabaa4f7662c285e22a2d7813c68b3
ypb <obpt@sina.com>
2020-02-01 22:01:04 +0800

    Add efcore.base project

---
+++ YS.EntityframeworkCore.Base/DatabaseFacadeExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Data;
+using System.Data.Common;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class DatabaseFacadeExtensions
+    {
+        public static int ExecuteStoredProcedureAsNonQuery(DatabaseFacade databaseFacade, string procedureName, params DbParameter[] parameters)
+        {
+            var cmd = databaseFacade.GetDbConnection().CreateCommand();
+            cmd.CommandText = procedureName;
+            cmd.Parameters.AddRange(parameters);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                databaseFacade.OpenConnection();
+                return cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                databaseFacade.CloseConnection();
+            }
+        }
+        public static object ExecuteStoredProcedureAsScalar(DatabaseFacade databaseFacade, string procedureName, params DbParameter[] parameters)
+        {
+            var cmd = databaseFacade.GetDbConnection().CreateCommand();
+            cmd.CommandText = procedureName;
+            cmd.Parameters.AddRange(parameters);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                databaseFacade.OpenConnection();
+                return cmd.ExecuteScalar();
+            }
+            finally
+            {
+                databaseFacade.CloseConnection();
+            }
+        }
+
+        public static void ExecuteStoredProcedureAsReader(DatabaseFacade databaseFacade, string procedureName, Action<DbDataReader> readAction, params DbParameter[] parameters)
+        {
+            var cmd = databaseFacade.GetDbConnection().CreateCommand();
+            cmd.CommandText = procedureName;
+            cmd.Parameters.AddRange(parameters);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                databaseFacade.OpenConnection();
+                readAction?.Invoke(cmd.ExecuteReader());
+            }
+            finally
+            {
+                databaseFacade.CloseConnection();
+            }
+        }
+    }
+}
