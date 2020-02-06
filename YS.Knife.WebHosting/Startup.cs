c56402a3416745f374ecdefe9e10639f6a3d65d0
ypb <obpt@sina.com>
2020-02-06 14:01:10 +0800

    disable auto validate api model

--- YS.Knife.WebHosting/Startup.cs
+++ YS.Knife.WebHosting/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,7 +13,11 @@ namespace Knife.WebHosting
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                // disable auto validate model
+                options.SuppressModelStateInvalidFilter = true;
+            });
 
         }
 
