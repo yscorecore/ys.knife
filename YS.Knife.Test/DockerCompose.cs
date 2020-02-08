0217b75e224a59d5ca7ed51ec54fc0aa9d0d6559
ypb <obpt@sina.com>
2020-02-08 17:54:42 +0800

    add Knife.Test base project

---
+++ YS.Knife.Test/DockerCompose.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace YS.Knife.Test
+{
+    public class DockerCompose
+    {
+        public static void Up(IDictionary<string, object> envs = null)
+        {
+
+            Exec("docker-compose", "up -d", envs);
+
+        }
+        public static void Down()
+        {
+            Exec("docker-compose", "down", null);
+        }
+        private static int Exec(string fileName, string arguments, IDictionary<string, object> envs)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+            };
+            if (envs != null)
+            {
+                foreach (var kv in envs)
+                {
+                    startInfo.Environment.Add(kv.Key, Convert.ToString(kv.Value));
+                }
+            }
+            var process = Process.Start(startInfo);
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+            {
+                throw new Exception($"Exec process return {process.ExitCode}.");
+            }
+            return process.ExitCode;
+        }
+    }
+}
---
+++ YS.Knife.Test/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace YS.Knife.Test
+{
+    public static class Utility
+    {
+        public static uint GetAvailableTcpPort(uint start = 1024, uint stop = IPEndPoint.MaxPort)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] endPoints = ipGlobalProperties.GetActiveTcpListeners();
+            for (uint i = start; i <= stop; i++)
+            {
+                if (!endPoints.Any(p => p.Port == i))
+                {
+                    return i;
+                }
+            }
+            throw new ApplicationException("Not able to find a free TCP port.");
+        }
+        const string fullCode = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$^&*()-+={}[]\\|?<>,./";
+
+        public static string NewPassword(int length = 32)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            StringBuilder passwordBuilder = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                passwordBuilder.Append(fullCode[random.Next(fullCode.Length)]);
+            }
+            return passwordBuilder.ToString();
+        }
+
+    }
+}
