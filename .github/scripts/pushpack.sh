144294d504acdce23e796b1aa9f057028c166001
ypb <obpt@sina.com>
2020-03-21 17:46:42 +0800

    sync repo template

--- .github/scripts/pushpack.sh
+++ .github/scripts/pushpack.sh
@@ -4,4 +4,4 @@ NUGET_APIKEY=$2
 for file in `ls $PUBLISH_FOLDER/*.nupkg`
 do
     dotnet nuget push $file -k $NUGET_APIKEY -s  https://api.nuget.org/v3/index.json --skip-duplicate
-done        
+done
