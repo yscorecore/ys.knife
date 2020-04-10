569b1aa3dca953a258c289cb51c53531b3b4f7aa
ypb <obpt@sina.com>
2020-04-10 23:54:20 +0800

    fix hosting unit test

--- test/YS.Knife.Hosting.UnitTest/KnifeFilterTest.cs
+++ test/YS.Knife.Hosting.UnitTest/KnifeFilterTest.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
+using YS.Knife.Hosting;
 using YS.Knife.TestData;
 
 namespace YS.Knife
@@ -12,67 +11,68 @@ namespace YS.Knife
         [TestMethod]
         public void ShouldGetInstanceIfNoFilters()
         {
-            var services = new ServiceCollection();
-            var filters = new Dictionary<string, string>
+            using (var host = new KnifeHost(
+                new Dictionary<string, object>
+                {
+                }))
             {
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(filters).Build();
-            var sp = services.RegisteKnifeServices(configuration).BuildServiceProvider();
-            var instance = sp.GetService<MyService1>();
-            Assert.IsNotNull(instance);
+                var instance = host.GetService<MyService1>();
+                Assert.IsNotNull(instance);
+            }
+
         }
         [TestMethod]
         public void ShouldNotGetInstanceWhenFilterAssemblyByName()
         {
-            var services = new ServiceCollection();
-            var filters = new Dictionary<string, string>
+            using (var host = new KnifeHost(
+               new Dictionary<string, object>
+               {
+                   ["Knife:Ignores:Assemblies:0"] = this.GetType().Assembly.GetName().Name
+               }))
             {
-                ["Knife:Ignores:Assemblies:0"] = this.GetType().Assembly.GetName().Name
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(filters).Build();
-            var sp = services.RegisteKnifeServices(configuration).BuildServiceProvider();
-            var instance = sp.GetService<MyService1>();
-            Assert.IsNull(instance);
+                var instance = host.GetService<MyService1>();
+                Assert.IsNull(instance);
+            }
         }
         [TestMethod]
         public void ShouldNotGetInstanceWhenFilterAssemblyByWildcard()
         {
-            var services = new ServiceCollection();
-            var filters = new Dictionary<string, string>
+            using (var host = new KnifeHost(
+              new Dictionary<string, object>
+              {
+                  ["Knife:Ignores:Assemblies:0"] = "*"
+              }))
             {
-                ["Knife:Ignores:Assemblies:0"] = "*"
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(filters).Build();
-            var sp = services.RegisteKnifeServices(configuration).BuildServiceProvider();
-            var instance = sp.GetService<MyService1>();
-            Assert.IsNull(instance);
+                var instance = host.GetService<MyService1>();
+                Assert.IsNull(instance);
+            }
         }
 
         [TestMethod]
         public void ShouldNotGetInstanceWhenFilterTypeByName()
         {
-            var services = new ServiceCollection();
-            var filters = new Dictionary<string, string>
+            using (var host = new KnifeHost(
+              new Dictionary<string, object>
+              {
+                  ["Knife:Ignores:Types:0"] = typeof(MyService1).FullName
+              }))
             {
-                ["Knife:Ignores:Types:0"] = typeof(MyService1).FullName
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(filters).Build();
-            var sp = services.RegisteKnifeServices(configuration).BuildServiceProvider();
-            var instance = sp.GetService<MyService1>();
-            Assert.IsNull(instance);
+                var instance = host.GetService<MyService1>();
+                Assert.IsNull(instance);
+            }
         }
         [TestMethod]
         public void ShouldNotGetInstanceWhenFilterTypeByWildcard()
         {
-            var services = new ServiceCollection();
-            var filters = new Dictionary<string, string>
+            using (var host = new KnifeHost(
+             new Dictionary<string, object>
+             {
+                 ["Knife:Ignores:Types:0"] = "*.MyService?"
+             }))
             {
-                ["Knife:Ignores:Types:0"] = "*.MyService?"
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(filters).Build();
-            var sp = services.RegisteKnifeServices(configuration).BuildServiceProvider();
-            var instance = sp.GetService<MyService1>();
-            Assert.IsNull(instance);
+                var instance = host.GetService<MyService1>();
+                Assert.IsNull(instance);
+            }
         }
     }
 }
