971477d5cf0bfd602e9ac05fe23160ed826c981b
ypb <obpt@sina.com>
2020-04-27 08:36:15 +0800

    add docker ignore

--- tool/YS.Knife.Wait.Compose.Ready/scripts/ready.sh
+++ tool/YS.Knife.Wait.Compose.Ready/scripts/ready.sh
@@ -1,5 +1,5 @@
 STATUS_FILE_PATH=/var/status/${STATUS_FILE-__docker_compose_ready__}
 
-echo 'all dependencies ready at ' > $STATUS_FILE_PATH 
+echo 'all dependencies ready at '+$(date "+%Y-%m-%d %H:%M:%S") > $STATUS_FILE_PATH 
 
 echo "docker compose ready, create status flag file '$STATUS_FILE_PATH'."
