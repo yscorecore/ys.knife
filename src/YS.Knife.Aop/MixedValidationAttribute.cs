08623d87e1314698f6ce5bf39cbe877354d28388
ypb <obpt@sina.com>
2020-05-31 14:44:59 +0800

    support mixed validation

---
+++ src/YS.Knife.Aop/MixedValidationAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace System.ComponentModel.DataAnnotations
+{
+    public abstract class MixedValidationAttribute : ValidationAttribute
+    {
+        private readonly ValidationAttribute[] validations;
+
+        public MixedValidationAttribute(params ValidationAttribute[] validations)
+        {
+            this.validations = validations;
+        }
+
+        public override bool IsValid(object value)
+        {
+            foreach (var validation in validations)
+            {
+                if (!validation.IsValid(value))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            foreach (var validation in validations)
+            {
+                var result = validation.GetValidationResult(value, validationContext);
+                if (result != ValidationResult.Success)
+                {
+                    return result;
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
---
+++ test/YS.Knife.Aop.UnitTest/MixedValidationAttributeTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+namespace YS.Knife.Aop.UnitTest
+{
+
+    [TestClass]
+    public class MixedValidationAttributeTest
+    {
+        [TestMethod]
+        public void ShouldValidateSuccessWhenGivenValidValue()
+        {
+            var entity = new DataEntity
+            {
+                Prop1 = "valid"
+            };
+            var validationContext = new ValidationContext(entity);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(entity, validationContext, validationResults, true);
+            Assert.AreEqual(0, validationResults.Count);
+        }
+        [TestMethod]
+        public void ShouldHasRequiredRule()
+        {
+            var entity = new DataEntity
+            {
+                Prop1 = null
+            };
+            var validationContext = new ValidationContext(entity);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(entity, validationContext, validationResults, true);
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The Prop1 field is required.", validationResults.First().ErrorMessage);
+        }
+        [TestMethod]
+        public void ShouldHasRegularExpressionRule()
+        {
+            var entity = new DataEntity
+            {
+                Prop1 = "Hello,World"
+            };
+            var validationContext = new ValidationContext(entity);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(entity, validationContext, validationResults, true);
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The field Prop1 must match the regular expression '^\\w+$'.", validationResults.First().ErrorMessage);
+        }
+    }
+
+    public class Rule1Attribute : MixedValidationAttribute
+    {
+        public Rule1Attribute() : base(
+            new RequiredAttribute(),
+            new RegularExpressionAttribute("^\\w+$"))
+        {
+        }
+    }
+
+    public class DataEntity
+    {
+        [Rule1]
+        public string Prop1 { get; set; }
+    }
+}
