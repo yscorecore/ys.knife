8f9c5c186c5d97f3f74ff68d18ae655375b15869
ypb <obpt@sina.com>
2020-06-06 22:43:39 +0800

    add aws configuration example

---
+++ example/configuration_aws_sm/App/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Options;
+using YS.Knife;
+using YS.Knife.HostedService;
+
+namespace App
+{
+    [HostedClass]
+    public class Service1 : TimerTickHostedService
+    {
+        public Service1(IOptionsMonitor<AppOptions> options)
+        {
+            this.Options = options;
+        }
+        public IOptionsMonitor<AppOptions> Options { get; }
+        protected override TimeSpan Interval => TimeSpan.FromSeconds(1);
+
+        protected override void OnException(Exception exception)
+        {
+        }
+
+        protected override void OnTick(CancellationToken state)
+        {
+            Console.WriteLine($"{DateTimeOffset.UtcNow} {Options.CurrentValue.GetHashCode():X} Str:{Options.CurrentValue.StrProp}\t\tInt:{Options.CurrentValue.IntProp}");
+        }
+    }
+}
