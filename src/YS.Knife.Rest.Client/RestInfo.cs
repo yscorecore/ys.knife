7d8fb46918ed2d99be18655531d56a286769c828
yangpengbo <obpt@sina.com>
2020-08-20 13:37:36 +0800

    rest client smoke test

--- src/YS.Knife.Rest.Client/RestInfo.cs
+++ src/YS.Knife.Rest.Client/RestInfo.cs
@@ -6,6 +6,14 @@ namespace YS.Knife.Rest.Client
 {
     public class RestInfo
     {
+        public RestInfo()
+        {
+
+        }
+        public RestInfo(String baseAddress)
+        {
+            this.BaseAddress = baseAddress;
+        }
         public string BaseAddress { get; set; }
         public TimeSpan Timeout { get; set; }
         public long MaxResponseContentBufferSize { get; set; }
---
+++ test/YS.Knife.Rest.Client.UnitTest/RestClientTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YS.Knife.Rest.Client.UnitTest
+{
+    [TestClass]
+    public class RestClientTest : YS.Knife.Hosting.KnifeHost
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var factory = this.GetService<IHttpClientFactory>();
+            var client = new RestClient("https://api.github.com/", factory.CreateClient());
+            var issues = await client.Get<IEnumerable<GitHubIssue>>("/repos/aspnet/AspNetCore.Docs/issues?state=open&sort=created&direction=desc",
+                new ApiArgument("Accept", ArgumentSource.FromHeader, "application/vnd.github.v3+json"),
+                new ApiArgument("User-Agent", ArgumentSource.FromHeader, "HttpClientFactory-Sample")
+            );
+            Assert.IsNotNull(issues);
+            Assert.IsTrue(issues.Count() > 0);
+        }
+
+
+        /// <summary>
+        /// A partial representation of an issue object from the GitHub API
+        /// </summary>
+        public class GitHubIssue
+        {
+            [JsonPropertyName("html_url")]
+            public string Url { get; set; }
+
+            [JsonPropertyName("title")]
+            public string Title { get; set; }
+
+            [JsonPropertyName("created_at")]
+            public DateTime Created { get; set; }
+        }
+    }
+}
