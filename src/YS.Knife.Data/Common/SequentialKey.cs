db31f87f29816d40972f9a2831e21e0ce903796c
pengbo.yang <obpt@sina.com>
2020-11-30 11:51:58 +0800

    sequencial key init random value

--- src/YS.Knife.Data/Common/SequentialKey.cs
+++ src/YS.Knife.Data/Common/SequentialKey.cs
@@ -9,9 +9,13 @@ namespace YS.Knife.Data
 {
     public static class SequentialKey
     {
-        private static readonly Random _random = new Random();
+        private static readonly Random _random = new Random((int)DateTime.Now.Ticks);
         private static int sequenceValue = 0;
         private static long lastTicks = 0;
+        /// <summary>
+        /// Create a new string key of length 24.
+        /// </summary>
+        /// <returns>A new key of length 24.</returns>
         public static string NewString()
         {
             // 15+3+6 
