f194bdf291d9105d0d76c1581de793e5bb5b7d78
pengbo.yang <obpt@sina.com>
2020-12-07 16:28:45 +0800

    add get entity default order info extension method

--- src/YS.Knife.Data/EntityKeyAttribute.cs
+++ src/YS.Knife.Data/EntityKeyAttribute.cs
@@ -11,54 +11,32 @@ namespace YS.Knife.Data
     public sealed class EntityKeyAttribute : System.Attribute
     {
         public string[] Keys { get; set; }
-        // See the attribute guidelines at
-        //  http://go.microsoft.com/fwlink/?LinkId=85236
-        readonly string positionalString;
-
-        // This is a positional argument
-        public EntityKeyAttribute(string positionalString)
+        public EntityKeyAttribute(params string[] keys)
         {
-            this.positionalString = positionalString;
-
-            // TODO: Implement code here
-            throw new System.NotImplementedException();
-        }
-
-        public string PositionalString
-        {
-            get { return positionalString; }
-        }
-    }
-
-    public sealed class DefaultOrderKeyAttribute : System.Attribute
-    {
-        public string PropName { get; set; }
-        public OrderType OrderType { get; set; } = OrderType.Asc;
-    }
-
-    public class LocalCache<TKey, TValue>
-    {
-        Dictionary<TKey, TValue> cachedData = new Dictionary<TKey, TValue>();
-
-        public TValue Get(TKey key, Func<TKey, TValue> createFunc)
-        {
-            lock (cachedData)
+            _ = keys ?? throw new ArgumentNullException(nameof(keys));
+            if (keys.Length < 0)
             {
-                if (cachedData.ContainsKey(key))
-                {
-                    return cachedData[key];
-                }
-                else
-                {
-                    return cachedData[key] = createFunc(key);
-                }
+                throw new ArgumentException("Must provide one key.");
             }
+            this.Keys = keys;
         }
-
     }
+
+    [System.AttributeUsage(System.AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public sealed class DefaultOrderByAttribute : System.Attribute
+    {
+        public DefaultOrderByAttribute(string propName, OrderType orderType = OrderType.Asc)
+        {
+            this.OrderInfo = new OrderItem(propName, orderType).ToString();
+        }
+        public string OrderInfo { get; set; }
+    }
+
+
     public static class EntityExtensions
     {
         static LocalCache<Type, PropertyInfo[]> KeyCache = new LocalCache<Type, PropertyInfo[]>();
+        static LocalCache<Type, OrderInfo> OrderByCache = new LocalCache<Type, OrderInfo>();
         public static PropertyInfo[] GetEntityKeyProps(this Type type)
         {
             return KeyCache.Get(type, (entityType) =>
@@ -76,6 +54,22 @@ namespace YS.Knife.Data
                     throw new InvalidOperationException($"Can not find entity key property from type '{entityType.FullName}', please use '{typeof(EntityKeyAttribute).FullName}' to define the keys.");
                 }
                 return new PropertyInfo[] { idProp };
+            });
+        }
+        public static OrderInfo GetEntityDefaultOrderInfo(this Type type)
+        {
+            return OrderByCache.Get(type, (entityType) =>
+            {
+                var orderByAttr = entityType.GetCustomAttribute<DefaultOrderByAttribute>();
+                if(orderByAttr!=null)
+                {
+                    return OrderInfo.Parse(orderByAttr.OrderInfo);
+                }
+                else
+                {
+                    var keyOrderItems = type.GetEntityKeyProps().Select(p=> new OrderItem(p.Name, OrderType.Asc));
+                    return new OrderInfo(keyOrderItems);
+                }
             });
         }
     }
---
+++ src/YS.Knife.Data/LocalCache.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace YS.Knife
+{
+    public class LocalCache<TKey, TValue>
+    {
+        Dictionary<TKey, TValue> cachedData = new Dictionary<TKey, TValue>();
+
+        public TValue Get(TKey key, Func<TKey, TValue> createFunc)
+        {
+            lock (cachedData)
+            {
+                if (cachedData.ContainsKey(key))
+                {
+                    return cachedData[key];
+                }
+                else
+                {
+                    return cachedData[key] = createFunc(key);
+                }
+            }
+        }
+    }
+}
