368a4716e2d37c3f419721449a148ba7949151e5
pengbo.yang <obpt@sina.com>
2020-12-18 01:22:29 +0800

    add redis project

---
+++ src/YS.Knife.Redis/RedisOptions.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+namespace YS.Knife.Redis
+{
+    [Options("Redis")]
+    public class RedisOptions
+    {
+        public string ConnectionString { get; set; } = "localhost";
+        public ConfigurationOptions Configuration { get; set; }
+    }
+}
---
+++ src/YS.Knife.Redis/ServiceRegister.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace YS.Knife.Redis
+{
+    public class ServiceRegister : IServiceRegister
+    {
+        public void RegisterServices(IServiceCollection services, IRegisterContext context)
+        {
+            services.AddSingleton<IConnectionMultiplexer>((sp) =>
+            {
+                var options = sp.GetService<RedisOptions>();
+                return CreateConnection(options);
+            });
+            services.AddSingleton<IDatabase>(sp =>
+            {
+                var connection = sp.GetRequiredService<IConnectionMultiplexer>();
+                return connection.GetDatabase();
+            });
+        }
+
+        private ConnectionMultiplexer CreateConnection(RedisOptions options)
+        {
+            return options.Configuration != null ?
+                    ConnectionMultiplexer.Connect(options.Configuration) :
+                    ConnectionMultiplexer.Connect(options.ConnectionString);
+        }
+
+       
+    }
+}
