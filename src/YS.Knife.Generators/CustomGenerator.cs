185f63899b899e8b42c0a1efc0c52a904721c9f5
ypb <obpt@sina.com>
2020-12-23 08:37:12 +0800

    add source generator project

---
+++ src/YS.Knife.Generators/CustomGenerator.cs
@@ -0,0 +1,27 @@
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace YS.Knife.Generators
+{
+[Generator]
+public class CustomGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context) {}
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        context.AddSource("myGeneratedFile.cs", SourceText.From(@"
+namespace GeneratedNamespace
+{
+    public class GeneratedClass
+    {
+        public static void GeneratedMethod()
+        {
+            // generated code
+        }
+    }
+}", Encoding.UTF8));
+    }
+}
+}
