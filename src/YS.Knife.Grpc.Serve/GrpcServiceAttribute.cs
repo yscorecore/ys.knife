4aeb113a2101b3b89160303beb4afe74b89dd754
pengbo.yang <obpt@sina.com>
2021-01-11 12:43:54 +0800

    format code style

--- src/YS.Knife.Grpc.Serve/GrpcServiceAttribute.cs
+++ src/YS.Knife.Grpc.Serve/GrpcServiceAttribute.cs
@@ -5,13 +5,13 @@ namespace YS.Knife.Grpc.Serve
 {
     public class GrpcServiceAttribute : YS.Knife.KnifeAttribute
     {
-        public GrpcServiceAttribute():base(null)
+        public GrpcServiceAttribute() : base(null)
         {
-            
+
         }
         public override void RegisterService(IServiceCollection services, IRegisterContext context, Type declareType)
         {
-            
+
         }
     }
 }
