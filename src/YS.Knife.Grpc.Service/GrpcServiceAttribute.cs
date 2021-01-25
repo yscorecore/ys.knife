2e296a951c9d677a79757f09b7ed5de3452ef2f0
pengbo.yang <obpt@sina.com>
2021-01-25 19:26:21 +0800

    add grpc client unit test project

--- src/YS.Knife.Grpc.Service/GrpcServiceAttribute.cs
+++ src/YS.Knife.Grpc.Service/GrpcServiceAttribute.cs
@@ -3,15 +3,8 @@
 namespace YS.Knife.Grpc
 {
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    public class GrpcServiceAttribute : Attribute
+    public sealed class GrpcServiceAttribute : Attribute
     {
-        //public GrpcServiceAttribute() : base(null)
-        //{
-
-        //}
-        //public override void RegisterService(IServiceCollection services, IRegisterContext context, Type declareType)
-        //{
-        //    // do nothing, just need map grpc service to endpoints
-        //}
+       
     }
 }
---
+++ test/YS.Knife.Grpc.Client.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YS.Knife.Grpc.Client.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
