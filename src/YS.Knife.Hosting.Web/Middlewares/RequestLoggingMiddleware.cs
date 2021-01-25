5019dbe001d15399b80cdb8623b94492b4bca70e
pengbo.yang <obpt@sina.com>
2021-01-26 01:33:34 +0800

    improve request logging middleware

--- src/YS.Knife.Hosting.Web/Middlewares/RequestLoggingMiddleware.cs
+++ src/YS.Knife.Hosting.Web/Middlewares/RequestLoggingMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Logging;
 
 namespace YS.Knife.Hosting.Web.Middlewares
@@ -25,13 +26,47 @@ namespace YS.Knife.Hosting.Web.Middlewares
             }
             finally
             {
-                _logger.LogInformation(
-                    "[{statusCode}] - [{time}] - {method} - {path}",
-                    context.Response?.StatusCode,
-                    DateTime.Now - start,
+
+                var action = GetLogingAction(context.Response.StatusCode);
+                action(_logger,
+                     context.Request.Protocol,
                     context.Request?.Method,
-                    context.Request?.Path.Value);
+                    context.Request.GetDisplayUrl(),
+                    (DateTime.Now - start).TotalSeconds,
+                    context.Response.StatusCode, null);
+
             }
+        }
+        private Action<ILogger, string, string, string, double, int, Exception> GetLogingAction(int statusCode)
+        {
+            if (statusCode >= 400)
+            {
+                return Logs.ErrorLog;
+            }
+            else if (statusCode >= 300)
+            {
+                return Logs.WarningLog;
+            }
+            else
+            {
+                return Logs.InfoLog;
+            }
+        }
+
+        private class Logs
+        {
+            public static Action<ILogger, string, string, string, double, int, Exception> InfoLog = LoggerMessage.Define<string, string, string, double, int>(
+                LogLevel.Information,
+                new EventId(1, nameof(InfoLog)),
+                "{protocol} {method} {url} took {time:f4} seconds, response code {statusCode}.");
+            public static Action<ILogger, string, string, string, double, int, Exception> WarningLog = LoggerMessage.Define<string, string, string, double, int>(
+               LogLevel.Warning,
+               new EventId(2, nameof(WarningLog)),
+               "{protocol} {method} {url} took {time:f4} seconds, response code {statusCode}.");
+            public static Action<ILogger, string, string, string, double, int, Exception> ErrorLog = LoggerMessage.Define<string, string, string, double, int>(
+             LogLevel.Error,
+             new EventId(3, nameof(WarningLog)),
+             "{protocol} {method} {url} took {time:f4} seconds, response code {statusCode}.");
         }
     }
 }
