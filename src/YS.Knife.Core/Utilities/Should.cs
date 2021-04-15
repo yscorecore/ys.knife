cc67c1e41d0cdc365c102f21550964449f8c04c8
yangpengbo <obpt@sina.com>
2021-04-15 23:58:51 +0800

    add should functions

---
+++ src/YS.Knife.Core/Utilities/Should.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace YS.Knife
+{
+    public static class Should
+    {
+        [DebuggerHidden]
+        public static void NotNull(object obj, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (obj is null)
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+
+        [DebuggerHidden]
+        public static void NotEmpty(string str, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (string.IsNullOrEmpty(str))
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+
+        [DebuggerHidden]
+        public static void NotBlank(string str, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+
+        [DebuggerHidden]
+        public static void NotEmpty<T>(IEnumerable<T> collection, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (collection is null || !collection.Any())
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+
+        [DebuggerHidden]
+        public static void BeTrue(bool condition, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (!condition)
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+
+        [DebuggerHidden]
+        public static void BeFalse(bool condition, Func<Exception> func)
+        {
+            _ = func ?? throw new ArgumentNullException(nameof(func));
+            if (condition)
+            {
+                var ex = func();
+                if (ex != null) throw func();
+            }
+        }
+    }
+}
---
+++ test/YS.Knife.Core.UnitTest/ShouldTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace YS.Knife
+{
+    [TestClass]
+    public class ShouldTest
+    {
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenNotNullCheckNotNullValue()
+        {
+            Should.NotNull("", () => new CodeException());
+            Should.NotNull(1, () => new CodeException());
+            Should.NotNull(new object(), () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ShouldThrowExceptionWhenNotNullCheckNullValue()
+        {
+            Should.NotNull(null, () => new InvalidOperationException());
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenNotEmptyCheckNotEmptyString()
+        {
+            Should.NotEmpty("a", () => new CodeException());
+            Should.NotEmpty(" ", () => new CodeException());
+            Should.NotEmpty("\t", () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenNotEmptyCheckEmptyString()
+        {
+            Should.NotEmpty("", () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenNotEmptyCheckNullString()
+        {
+            Should.NotEmpty(null, () => new CodeException());
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenNotEmptyCheckNotEmptyCollection()
+        {
+            Should.NotEmpty(new[] { 1 }, () => new CodeException());
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldNotThrowExceptionWhenNotEmptyCheckEmptyCollection()
+        {
+            Should.NotEmpty(new string[0], () => new CodeException());
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldNotThrowExceptionWhenNotEmptyCheckNullCollection()
+        {
+            IEnumerable<string> enumerable = null;
+            Should.NotEmpty(enumerable, () => new CodeException());
+        }
+
+
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenNotBlankCheckNotBlankString()
+        {
+            Should.NotBlank("a", () => new CodeException());
+            Should.NotBlank(" a ", () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenNotBlankCheckEmptyString()
+        {
+            Should.NotBlank("", () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenNotBlankCheckNullString()
+        {
+            Should.NotBlank(null, () => new CodeException());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenNotBlankCheckBlankString()
+        {
+            Should.NotBlank(" \t ", () => new CodeException());
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenBeTrueCheckTrueValue()
+        {
+            Should.BeTrue(true, () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenBeTrueCheckFalseValue()
+        {
+            Should.BeTrue(false, () => new CodeException());
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenBeFalseCheckFalseValue()
+        {
+            Should.BeFalse(false, () => new CodeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CodeException))]
+        public void ShouldThrowExceptionWhenBeFalseCheckTrueValue()
+        {
+            Should.BeFalse(true, () => new CodeException());
+        }
+    }
+}
