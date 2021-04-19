0f1f490fb8f17bb90434fec47f590482964fd6e9
yangpengbo <obpt@sina.com>
2021-04-19 18:14:54 +0800

    limit info include page info

--- src/YS.Knife.Data/Data/ILimit.cs
+++ src/YS.Knife.Data/Data/ILimit.cs
@@ -8,12 +8,12 @@ namespace YS.Knife.Data
         int Limit { get; }
         int TotalCount { get; }
         bool HasNext { get; }
+        bool CanToPage => this.Limit > 0 && this.Offset % this.Limit == 0;
+        int PageSize => this.Limit;
+        int PageIndex => this.Limit > 0 ? this.Offset / this.Limit + 1 : 1;
     }
     public interface ILimitData<TData> : ILimit
     {
         List<TData> ListData { get; }
     }
-
-
-
 }
