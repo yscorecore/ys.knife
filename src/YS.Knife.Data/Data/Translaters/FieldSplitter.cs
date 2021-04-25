768dc35e8fdb535cd9255f229bcefad361ac56ac
yangpengbo <obpt@sina.com>
2021-04-25 19:34:54 +0800

    add field splitter

---
+++ src/YS.Knife.Data/Data/Translaters/FieldSplitter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+
+namespace YS.Knife.Data.Translaters
+{
+    public class FieldSplitter
+    {
+        private static Regex path = new Regex(@"^\.[\w+]+$");
+        public FieldPath Split(string filedName)
+        {
+            if (string.IsNullOrWhiteSpace(filedName))
+            {
+                throw new ArgumentException($"Field name to split should not be blank.");
+            }
+
+            bool inFunction = false;
+            
+           
+            return null;
+        }
+    }
+
+    public class FieldPath
+    {
+        public string Field { get; set; }
+
+        public string FuncName { get; set; }
+        
+        public List<FieldPath> SubPaths { get; set; }
+    }
+}
---
+++ src/YS.Knife.Data/Data/Translaters/FilterTranslater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace YS.Knife.Data.Translaters
+{
+    public class FilterTranslater
+    {
+        public FilterInfo Translate<TFrom, TTo>(FilterInfo filterInfo)
+        {
+            return Translate(typeof(TFrom), typeof(TTo), filterInfo);
+        }
+        public FilterInfo Translate(Type from, Type to, FilterInfo filterInfo)
+        {
+            return filterInfo;
+        }
+    }
+}
