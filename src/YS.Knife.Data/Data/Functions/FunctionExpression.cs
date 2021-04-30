725812b1a4d6bbf492d1aaa982ac19da6f82af8a
yangpengbo <obpt@sina.com>
2021-04-30 19:00:33 +0800

    impl filter translater

---
+++ src/YS.Knife.Data/Data/Functions/FunctionExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YS.Knife.Data.Functions
+{
+    public abstract class FunctionExpression
+    {
+        private static readonly Dictionary<string, FunctionExpression> AllFunctions =
+            AppDomain.CurrentDomain.FindInstanceTypesByBaseType<FunctionExpression>().ToDictionary(type => type.Name,
+                type => Activator.CreateInstance(type) as FunctionExpression,
+                StringComparer.InvariantCultureIgnoreCase);
+           
+
+       public static FunctionExpression GetFunctionByName(string funcName)
+       {
+           if (AllFunctions.TryGetValue(funcName, out FunctionExpression func))
+           {
+               return func;
+           }
+
+           return null;
+       }
+    }
+}
