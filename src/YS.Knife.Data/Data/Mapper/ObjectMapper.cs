f4eb07b83fcccbbcfc1626e7b3a6f4981cff9529
yangpengbo <obpt@sina.com>
2021-05-10 10:17:20 +0800

    add object mapper

---
+++ src/YS.Knife.Data/Data/Mapper/ObjectMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace YS.Knife.Data.Mapper
+{
+    public class ObjectMapper:List<PropMapper>
+    {
+        public ObjectMapper(Type fromType,Type toType)
+        {
+            
+        }
+        internal ObjectMapper(Type fromType,Type toType,string[] basicProps)
+        {
+            
+        }
+        public ObjectMapper AddProperty(string fromName, string toName)
+        {
+            return null;
+        }
+
+        // 只过滤指定字段，如果为空，则查询全部
+        public ObjectMapper Filter(string[] fields)
+        {
+            if (fields != null && fields.Length > 0)
+            {
+                return this;
+            }
+            return this;
+        }
+    }
+
+    public class ObjectMapper<TFrom, TTo>
+    {
+        public Expression<Func<TFrom, TTo>> ToExpression()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/YS.Knife.Data/Data/Mapper/PropMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace YS.Knife.Data.Mapper
+{
+    public class PropMapper
+    {
+        public Type FromType { get; }
+        public PropertyInfo FromProperty { get; }
+        public string FromName { get;  }
+               
+        public Type ToType { get; }
+        public Expression ToExpression { get; }
+        public string ToPath { get; set; }
+    }
+
+    public class NestedObjectMapper
+    {
+    }
+
+    public class CollectionMapper
+    {
+        
+    }
+}
