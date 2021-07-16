2602a488ce4cfb797f9cb2d7b31c1681129bf1be
pengbo.yang <obpt@sina.com>
2021-07-17 02:47:43 +0800

    support elastic search

---
+++ src/YS.Knife.Elasticsearch/ElasticOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace YS.Knife.Elasticsearch
+{
+    [Options]
+    public class ElasticOptions
+    {
+        [Required]
+        [MaxLength(1000)]
+        [MinLength(1)]
+
+        public List<string> Urls { get; set; } = new List<string> { "http://localhost:9200" };
+        [RegularExpression("^\\w+$")]
+        [Required(AllowEmptyStrings =false)]
+        public string DefaultIndex { get; set; } = "defaultindex";
+    }
+}
---
+++ src/YS.Knife.Elasticsearch/ServiceRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Elasticsearch.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System.Linq;
+namespace YS.Knife.Elasticsearch
+{
+    class ServiceRegister : IServiceRegister
+    {
+        public void RegisterServices(IServiceCollection services, IRegisterContext context)
+        {
+            services.AddSingleton<ElasticClient>((sp) =>
+            {
+                var options = sp.GetRequiredService<ElasticOptions>();
+                var allUrls= (options.Urls ?? Enumerable.Empty<string>()).Select(p => new Uri(p));
+                var pool = new StaticConnectionPool(allUrls);
+                var settings = new ConnectionSettings(pool)
+                .DefaultIndex(options.DefaultIndex);
+                return new ElasticClient(settings);
+
+            });
+            services.AddSingleton<IElasticClient>(sp => sp.GetRequiredService<ElasticClient>());
+        }
+    }
+}
