684ba60293c557fecd2e8802b19f81f82b1b716c
pengbo.yang <obpt@sina.com>
2021-07-26 23:19:08 +0800

    fix const value test

--- test/YS.Knife.Data.UnitTest/Filter/FilterInfo2ParserTest.cs
+++ test/YS.Knife.Data.UnitTest/Filter/FilterInfo2ParserTest.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using YS.Knife.Data.Query;
