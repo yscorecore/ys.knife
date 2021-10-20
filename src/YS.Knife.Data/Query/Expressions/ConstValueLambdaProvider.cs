94b70fb89d508a6082b0ac8235845f67c87e17a3
pengbo.yang <obpt@sina.com>
2021-10-20 16:57:14 +0800

    create value converter

--- src/YS.Knife.Data/Query/Expressions/ConstValueLambdaProvider.cs
+++ src/YS.Knife.Data/Query/Expressions/ConstValueLambdaProvider.cs
@@ -27,87 +27,8 @@ namespace YS.Knife.Data.Query.Expressions
         public LambdaExpression GetLambda(ParameterExpression parameter, Type targetType)
         {
             _ = targetType ?? throw new ArgumentNullException(nameof(targetType));
-            var constExp = Expression.Constant(ChangeType(this.ConstValue, targetType), targetType);
+            var constExp = Expression.Constant(ValueConverter.Instance.ChangeType(this.ConstValue, targetType), targetType);
             return Expression.Lambda(typeof(Func<,>).MakeGenericType(SourceType, targetType), constExp, parameter);
-        }
-
-        private object ChangeType(object value, Type valueType)
-        {
-            if (value == null)
-            {
-                return valueType.DefaultValue();
-            }
-
-            try
-            {
-                var originType = Nullable.GetUnderlyingType(valueType) ?? valueType;
-
-                if (originType.IsEnum)
-                {
-                    return ChangeToEnumType(value, originType);
-                }
-
-                if (originType == typeof(DateTime) || originType == typeof(DateTimeOffset))
-                {
-                    if (value is double or long or int or float or decimal)
-                    {
-                        // support unix time stamp
-                        long longValue = (long)ChangeType(value, typeof(long));
-                        var dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(longValue);
-                        return GetDateTimeByTargetType(originType, dateTimeOffset);
-                    }
-                    if (value is string str)
-                    {
-                        var dateTimeOffset = DateTimeOffset.Parse(str);
-                        return GetDateTimeByTargetType(originType, dateTimeOffset);
-                    }
-                }
-
-
-                if (value is IConvertible && typeof(IConvertible).IsAssignableFrom(originType))
-                {
-                    return Convert.ChangeType(value, originType);
-                }
-                else
-                {
-                    var converter = TypeDescriptor.GetConverter(originType);
-                    return converter.ConvertFrom(value);
-                }
-            }
-            catch (QueryExpressionException)
-            {
-                throw;
-            }
-            catch (Exception ex)
-            {
-                throw ExpressionErrors.ConvertValueError(value, valueType, ex);
-            }
-        }
-
-
-
-        private static object GetDateTimeByTargetType(Type targetType, DateTimeOffset dateTimeOffset)
-        {
-            if (targetType == typeof(DateTimeOffset))
-            {
-                return dateTimeOffset;
-            }
-            else
-            {
-                return dateTimeOffset.DateTime;
-            }
-        }
-
-        private static object ChangeToEnumType(object value, Type enumType)
-        {
-            if (value is string str)
-            {
-                return Enum.Parse(enumType, str, true);
-            }
-            else
-            {
-                return Enum.ToObject(enumType, Convert.ChangeType(value, Enum.GetUnderlyingType(enumType)));
-            }
         }
     }
 }
---
+++ src/YS.Knife.Data/Query/Expressions/ValueConverter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using YS.Knife.Data.Query.Expressions;
+
+namespace YS.Knife.Data.Query
+{
+    internal class ValueConverter
+    {
+        public static ValueConverter Instance { get; } = new ValueConverter();
+
+        public IList ChangeListItemType(IList source, Type itemType)
+        {
+            return (source?? Array.Empty<object>()).Cast<object>().Select(p => ChangeType(p, itemType)).ToArray();
+        }
+
+        public object ChangeType(object value, Type valueType)
+        {
+            if (value == null)
+            {
+                return valueType.DefaultValue();
+            }
+
+            try
+            {
+                var originType = Nullable.GetUnderlyingType(valueType) ?? valueType;
+
+                if (originType.IsEnum)
+                {
+                    return ChangeToEnumType(value, originType);
+                }
+
+                if (originType == typeof(DateTime) || originType == typeof(DateTimeOffset))
+                {
+                    if (value is double or long or int or float or decimal)
+                    {
+                        // support unix time stamp
+                        long longValue = (long)ChangeType(value, typeof(long));
+                        var dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(longValue);
+                        return GetDateTimeByTargetType(originType, dateTimeOffset);
+                    }
+                    if (value is string str)
+                    {
+                        var dateTimeOffset = DateTimeOffset.Parse(str);
+                        return GetDateTimeByTargetType(originType, dateTimeOffset);
+                    }
+                }
+
+
+                if (value is IConvertible && typeof(IConvertible).IsAssignableFrom(originType))
+                {
+                    return Convert.ChangeType(value, originType);
+                }
+                else
+                {
+                    var converter = TypeDescriptor.GetConverter(originType);
+                    return converter.ConvertFrom(value);
+                }
+            }
+            catch (QueryExpressionException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ExpressionErrors.ConvertValueError(value, valueType, ex);
+            }
+        }
+        
+        private static object GetDateTimeByTargetType(Type targetType, DateTimeOffset dateTimeOffset)
+        {
+            if (targetType == typeof(DateTimeOffset))
+            {
+                return dateTimeOffset;
+            }
+            else
+            {
+                return dateTimeOffset.DateTime;
+            }
+        }
+
+        private static object ChangeToEnumType(object value, Type enumType)
+        {
+            if (value is string str)
+            {
+                return Enum.Parse(enumType, str, true);
+            }
+            else
+            {
+                return Enum.ToObject(enumType, Convert.ChangeType(value, Enum.GetUnderlyingType(enumType)));
+            }
+        }
+    }
+}
